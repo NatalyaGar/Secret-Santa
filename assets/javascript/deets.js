a32d79b36a50cf7d7706824c650990c563be55ac
LuvWubs <iydkwtmiwt@gmail.com>
2018-03-23 21:35:24 -0600

    stop obj creation if invalid budget entered

--- assets/javascript/deets.js
+++ assets/javascript/deets.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  // ajax request for map API & currency API (& calendar?)
+  //NOTE ajax request for map API & currency API (& calendar?)
   // $.ajax({
   //   url: '',
   //   method: 'GET'
@@ -20,17 +20,25 @@ $(document).ready(function(){
   firebase.initializeApp(config);
   var database = firebase.database();
 
+
+    // var newUser ={
+    //     name: userName,
+    //     // passw: password,
+    //     // newPassw: newPassword,
+    //     // varifPassw: varifyPassword,
+    // };
+    //
+    //     //Upload user data to the database
+    //     database.ref().push(newUser);
+
 //NOTE add properties to Guest constructor thru protype?????
   // function logIt(name, pw) {
   //
   // }
 
-  //.................uncomment this from here.......................
-  function Guest(
-    // name, gifts,
-    budget, date) {
-    // this.name = name;
-    // this.gifts = gifts;
+  function Guest(name, gifts, budget, date) {
+    this.name = name;
+    this.gifts = gifts;
     this.budget = budget;
     this.date = date;
   }
@@ -43,11 +51,11 @@ $(document).ready(function(){
 
 $('#sendIt').click(function() {
     console.log('button clicked!');
-    // var name = $("#name").val().trim();
-    // var gift1 =$('#gift1').val().trim();
-    // var gift2 =$('#gift2').val().trim();
-    // var gift3 =$('#gift3').val().trim();
-    // var gifts = [gift1, gift2, gift3];
+    var name = $("#name").val().trim();
+    var gift1 =$('#gift1').val().trim();
+    var gift2 =$('#gift2').val().trim();
+    var gift3 =$('#gift3').val().trim();
+    var gifts = [gift1, gift2, gift3];
     var minAmt = $('#min').val().trim();
     var maxAmt = $('#max').val().trim();
     var budget = [minAmt, maxAmt];
@@ -59,33 +67,34 @@ $('#sendIt').click(function() {
     var date = $('#dateDeet').val().trim();
     isValidDate(date);
 
-    // if ((name == '') || (gift1 == '') || (maxAmt == '') || (date == '')) {
-    //
-    //   console.log('Please enter all of the following info: name, gift, max amount, date');
-    // // } else if ( date === 'number') {
-    // //   console.log('please enter a valid date');
-    // } else {
-      // sendIt(name, gifts, budget, date);
-      sendIt(budget, date);
-    // }
-    //.................uncomment this to here.......................
+    if ((name == '') || (gift1 == '') || (date == '')) {
+      console.log('Please enter all of the following info: name, gift, date');
+    } else {
+      sendIt(name, gifts, budget, date);
+    }
   })
 
   function isValidAmt(budget) {
     if (isNaN(budget[0])) {
       console.log('min number is NOT valid, please enter a number');
+      //NOTE need to stop code here if number is not valid
+      return;
     } else if (isNaN(budget[1])) {
       console.log('max number is NOT valid, please enter a number');
+      //NOTE need to stop code here if number is not valid
+      return;
     }
     if (budget[0] == '') {
       console.log('min budget not listed ');
-      return budget[0] = '0';
+      return budget[0] += budget[1].toString();
     } else if (budget[1] == '') {
       console.log('max budget not listed ');
-      return budget[1] = '0';
+      return budget[1] += budget[0].toString();
     }
     if (budget[0] > budget[1]) {
       console.log('please check that your maximum budget is larger than your minimum budget');
+      //NOTE need to stop code here if budget is not valid
+      return;
     }
   }
 
@@ -104,51 +113,27 @@ $('#sendIt').click(function() {
         $("#dateDeet").val('');
       } else {
         console.log('Time travel has not been invented yet. Please enter a future date!');
+        //NOTE need to stop code here if date is not valid
+        return;
       }
     } else {
       console.log('date is NOT valid');
+      //NOTE need to stop code here if date is not valid
+      return;
     }
   }
 
-  // var newUser ={
-  //     name: userName,
-  //     // passw: password,
-  //     // newPassw: newPassword,
-  //     // varifPassw: varifyPassword,
-  // };
-  //
-  //     //Upload user data to the database
-  //     database.ref().push(newUser);
-
     function avgIt(budget) {
       console.log('avgIt budget: ', budget);
-//.................uncomment this from here.......................
-      // if (budget[0] > 0 && budget[1] > 0) {
-      //   console.log('budget is valid');
-      // }
-      // if (budget[0] <= budget[1]) {
-      //   let avg = budget[1] - budget[0];
-      //   console.log('the Guests budget avg is: ', avg);
-      //   return budget.push(avg);
-      //   //   console.log('min budget < max budget, good job!');
-      //   // } else if () {
-      //   //   console.log('please enter min/max budgets in correct order');
-      // }
-//................uncomment this to here....................
+        let avg = ((parseInt(budget[0]) + parseInt(budget[1])) / budget.length).toString();
+        console.log('the Guests budget avg is: ', avg);
+        return budget.push(avg);
     }
 
-//.................uncomment this from here.......................
-  // function sendIt(name, gifts, budget, date) {
-  //   var user = new Guest(name, gifts, budget, date);
-  //   console.log('guest profile deets:::', user);
-  //   database.ref().push(user);
-  // }
-//................uncomment this to here....................
-
-  function sendIt(budget, date) {
+  function sendIt(name, gifts, budget, date) {
     console.log('sendIt budget: ', budget);
     avgIt(budget);
-    var user = new Guest(budget, date);
+    var user = new Guest(name, gifts, budget, date);
     console.log('guest profile deets:::', user);
     database.ref().push(user);
   }
