ca6f34a999343a881740c407eaa1e1af76c4278d
LuvWubs <iydkwtmiwt@gmail.com>
2018-03-28 20:07:09 -0600

    pull request

--- assets/javascript/deets.js
+++ assets/javascript/deets.js
@@ -7,23 +7,23 @@
 
 
 // Template variable clones the div to dynamically add new participants
-// var config = {
-// 	apiKey: "AIzaSyDv2FsrJadHox9lm9ccXWDfqKtAipU4u_M",
-// 	authDomain: "secretsantaprototype-1ea82.firebaseapp.com",
-// 	databaseURL: "https://secretsantaprototype-1ea82.firebaseio.com",
-// 	projectId: "secretsantaprototype-1ea82",
-// 	storageBucket: "secretsantaprototype-1ea82.appspot.com",
-// 	messagingSenderId: "462928145575"
-// };
-
 var config = {
-  apiKey: "AIzaSyDLN3xvbcjFVqcg9yDl2-0sBaR_CNt0SvY",
-  authDomain: "mapapi-f40ea.firebaseapp.com",
-  databaseURL: "https://mapapi-f40ea.firebaseio.com",
-  projectId: "mapapi-f40ea",
-  storageBucket: "mapapi-f40ea.appspot.com",
-  messagingSenderId: "890218358654"
+	apiKey: "AIzaSyDv2FsrJadHox9lm9ccXWDfqKtAipU4u_M",
+	authDomain: "secretsantaprototype-1ea82.firebaseapp.com",
+	databaseURL: "https://secretsantaprototype-1ea82.firebaseio.com",
+	projectId: "secretsantaprototype-1ea82",
+	storageBucket: "secretsantaprototype-1ea82.appspot.com",
+	messagingSenderId: "462928145575"
 };
+
+// var config = {
+//   apiKey: "AIzaSyDLN3xvbcjFVqcg9yDl2-0sBaR_CNt0SvY",
+//   authDomain: "mapapi-f40ea.firebaseapp.com",
+//   databaseURL: "https://mapapi-f40ea.firebaseio.com",
+//   projectId: "mapapi-f40ea",
+//   storageBucket: "mapapi-f40ea.appspot.com",
+//   messagingSenderId: "890218358654"
+// };
 firebase.initializeApp(config);
 
 // Create a variable to reference the database
@@ -35,7 +35,7 @@ $(document).ready(function(){
   $("#deets-container").hide();
   $("#containerSeeRecipient").hide();
    $("#ChristmasTwo-button").hide();
-  
+
      //Christmas Cheer button
      $("#Christmas-button").on("click", function() {
        $("#Christmas-button").hide();
@@ -48,19 +48,19 @@ $(document).ready(function(){
                   url: queryURL,
                   method: "GET"
                   })
-          // stick the image on the screen 
+          // stick the image on the screen
           .then(function(response) {
               console.log(response);
               // var imageUrl = response.data.image_original_url;
               var imageUrl = response.data.images.fixed_height_downsampled.url
-              // make a variable, to get the data back from the api 
+              // make a variable, to get the data back from the api
               // var christmasImage = $('<img style="width: 460px;">');
               var christmasImage = $('<img style="width:100%;">');
               // create a new image element
               christmasImage.attr("src", imageUrl);
               $("#images").empty();
               $("#images").prepend(christmasImage);
-             
+
           }); //response function close
       }); //on click function close
 
@@ -74,38 +74,38 @@ $(document).ready(function(){
                 url: queryURL,
                 method: "GET"
                 })
-        // stick the image on the screen 
+        // stick the image on the screen
         .then(function(response) {
             console.log(response);
             // var imageUrl = response.data.image_original_url;
             var imageUrl = response.data.images.fixed_height_downsampled.url
-            // make a variable, to get the data back from the api 
+            // make a variable, to get the data back from the api
             // var christmasImage = $('<img style="width: 460px;">');
             var christmasImage = $('<img style="width:100%;">');
             // create a new image element
             christmasImage.attr("src", imageUrl);
             $("#images").empty();
             $("#images").prepend(christmasImage);
-           
+
         }); //response function close
     }); //on click function close
-  
+
 
        //Audio Element
        var audioElement = document.createElement("audio");
        audioElement.setAttribute("src", "music/WhiteChristmas.mp3");
-      
+
      // music play button
        $("#musicPlay").on("click", function() {
-        audioElement.play();                                    
+        audioElement.play();
            });
        //music pause button
        $("#musicPause").on("click", function() {
             audioElement.pause();
             // audioElementV.pause();
           });
- 
- 
+
+
 
     function Guest(name, pw, gifts, budget, date) {
       this.name = name;
@@ -115,16 +115,16 @@ $(document).ready(function(){
       this.date = date;
     }
 
-    
+
       $('#sendIt').on("click", function(event) {
       event.preventDefault();
-   
+
       console.log("click");
       //Modal message show when click submit button
-      $('#myModal').modal(); 
+      $('#myModal').modal();
       $("#modalContainer").show()
-         
-   
+
+
       var name = $("#name").val();
       // var pw = $("#password-input").val();
       var gift1 =$('#gift1').val().trim();
@@ -139,11 +139,11 @@ $(document).ready(function(){
       console.log('gift array: ', gifts);
       console.log('budget: ', budget);
       console.log('date chosen: ', date);
-    
+
       sendIt(name, gifts, budget, date);
-    
+
     })
-    
+
     function sendIt(name, gifts, budget, date) {
       var user = new Guest(name, gifts, budget, date);
       console.log('is this working???', user);
@@ -156,7 +156,7 @@ $(document).ready(function(){
     //  $("#deets-container").hide();
     $("#deets-container").show()
     //  $("#containerSeeRecipient").show();
-     
+
 
 
       //hide deets section on click submit button
@@ -173,34 +173,34 @@ $(document).ready(function(){
     //   $("#myModalError").modal();
     //   $("#modalContainerError").show();
     //   $("#deetsLeftContainer").show();
-     
+
     // }
     // else{
-    //   $('#myModal').modal(); 
+    //   $('#myModal').modal();
     //   $("#modalContainer").show()
     //   $("#deetsLeftContainer").hide();
     //   $("#deets-container").show();
     // }
 
 
-   
+
 
 
     $("#submitMe").on("click", function(event) {
       event.preventDefault();
-   
+
       console.log("Pair");
       //Modal message show when click Pair button
-      $('#myModalPair').modal(); 
+      $('#myModalPair').modal();
       $("#modalContainerPair").show()
     });
 
     $("#haveGroupBtn").on("click", function(event) {
       event.preventDefault();
-   
+
       console.log("LogIn");
       //Modal message show when click Log in to view who's Santa you are button
-      $('#myModalLogIn').modal(); 
+      $('#myModalLogIn').modal();
       $("#modalContainerLogIn").show()
     });
 
@@ -211,16 +211,16 @@ $(document).ready(function(){
     var template = $('#sections').clone();
 
     var sectionsCount = 0;
-    
+
     var userName = [];
-    
+
     var indexID = -1;
-    
-    
+
+
     // When user clicks .addMe button, adds new participant
     $('body').on('click', '.addMe', function () {
       sectionsCount++;
-    
+
       // Loop through each input
       var section = template.clone().find(':input').each(function () {
         var newId = this.id.slice(0, -1) + sectionsCount;
@@ -233,7 +233,7 @@ $(document).ready(function(){
         .appendTo('#sections');
       return false;
     });
-    
+
     //Main function for Secret Santa web app/////////////////////////////////////////////////////
     // Stores data from form in array and uses that data to pair participants
     $('#submitMe').on('click', function () {
@@ -243,9 +243,9 @@ $(document).ready(function(){
       var gifts1 = [];
       var gifts2 = [];
       var gifts3 = [];
-    
-    
-    
+
+
+
       // Create an array for each participant within the participants array
       $('#secretSantaForm').find(".section").each(function () {
         indexID++;
@@ -266,7 +266,7 @@ $(document).ready(function(){
         gifts1.push(gift1);
         gifts2.push(gift2);
         gifts3.push(gift3);
-    
+
         // console.log(gifts1);
         // console.log(gifts2);
         // console.log(gifts3);
@@ -275,56 +275,56 @@ $(document).ready(function(){
         console.log("Individual Participant: ");
         // getIndex(participantsNotShuffled,indexID);
         console.log(participantsNotShuffled[indexID]);
-    
+
       });
-    
+
       console.log("Here's the  aray: " + participants);
       participants = shuffle(participants);
       console.log("Here's the  shuffled aray: " + participants);
       participants.forEach(matchParticipants);
       console.log("Here are the matched participants");
       console.log(participants);
-    
-    
+
+
       for (i = 0; i < participants.length; i++) {
         var name = participants[i][0];
         var matched = participants[i];
-    
-    
+
+
         if (participants[0][i] === undefined) {
           console.log(participants[i][0]);
           var index = functiontofindIndexByKeyValue(participantsNotShuffled, participants[i][0]);
           var gifts = { item1: gifts1[index], item2: gifts2[index], item3: gifts3[index] };
-    
+
         } else {
           console.log(participants[0][i]);
           var index = functiontofindIndexByKeyValue(participantsNotShuffled, participants[i][0]);
           var gifts = { item1: gifts1[index], item2: gifts2[index], item3: gifts3[index] };
         }
-    
+
         database.ref().push({
           name: name,
           recipientPair: matched,
           recipientGifts: gifts
-    
+
         });
       }
     })
-    
-    
+
+
     /**
-     * Function that will randomly shuffle an array. 
+     * Function that will randomly shuffle an array.
      */
     function shuffle(array) {
       var currentIndex = array.length, temporaryValue, randomIndex;
-    
+
       // While there remain elements to shuffle...
       while (0 !== currentIndex) {
-    
+
         // Pick a remaining element...
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-    
+
         // And swap it with the current element.
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
@@ -332,8 +332,8 @@ $(document).ready(function(){
       }
       return array;
     };
-    
-    // Function to match participants with recipients 
+
+    // Function to match participants with recipients
     // Matches current participant in array with the next participant in array
     // Unless they are the last, in which case matches them with the first participant in array
     function matchParticipants(participant, index, array) {
@@ -348,18 +348,18 @@ $(document).ready(function(){
         // console.log("Else Block: " + index);
       }
     };
-    
-    
+
+
     function functiontofindIndexByKeyValue(arraytosearch, valuetosearch) {
-    
+
       for (var i = 0; i < arraytosearch.length; i++) {
-    
+
         if (arraytosearch[i] == valuetosearch) {
           return i;
         }
       }
       return null;
-    }; 
+    };
     //Mark's code for Participant's names and wish list close
 
 
@@ -367,7 +367,7 @@ $(document).ready(function(){
 
 //Mark's code Pair the recipients start
 
-   
+
 
 var fbparticipant = [];
 var count = "";
@@ -420,5 +420,5 @@ $('body').on('click', '.getMe', function () {
 
 
 
-    
+
     });
